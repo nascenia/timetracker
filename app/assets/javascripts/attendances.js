683c91f9bf8af2570e7e7257c6ec61f8d9ea63dd
Khalid <khalid@nascenia.com>
2014-11-03 11:30:13 +0600

    displayed 50 entries as default and removed auto page refresh.

--- app/assets/javascripts/attendances.js
+++ app/assets/javascripts/attendances.js
@@ -1,16 +1,9 @@
 $(document).ready(function() {
     $('#monthly-attendance-table').dataTable({
-        "language": {
-            "lengthMenu": 'Display <select class ="form-control" id="display-records">'+
-                '<option value="50" >50</option>'+
-                '<option value="100">100</option>'+
-                '<option value="-1" selected>All</option>'+
-                '</select> records'
-        },
         dom: 'T<"clear">lfrtip',
         tableTools: {
             "sSwfPath": "http://cdn.datatables.net/tabletools/2.2.2/swf/copy_csv_xls_pdf.swf",
-            "aButtons": [ "csv", "xls", "pdf", "print" ]
+            "aButtons": ["pdf", "print" ]
         }
 
     });
