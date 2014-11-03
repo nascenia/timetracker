c138baaafbd9d1e81c470ba60920924cfc4ea599
Khalid <khalid@nascenia.com>
2014-11-03 17:11:50 +0600

    Displayed DISTINCT user Count as total present in Office.

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -4,15 +4,13 @@ class Attendance < ActiveRecord::Base
   USUAL_OFFICE_TIME = "10:00"
 
   scope :by_month, ->(month) {where("MONTH(datetoday) =?", month)}
+  scope :todays_attendance_summary, ->(date) {where("datetoday =? ", date).order(:in)}
+  scope :total_employee_present, ->(date) {where("datetoday =?", date).group(:user_id).count}
 
   def update_out_time
     self.update_attribute(:out, Time.now.to_s(:time))
     total_hours = ((self.out.to_time - self.in.to_time) / 1.hour).round(2)
     self.update_attribute(:total_hours, total_hours)
-  end
-
-  def self.todays_attendance_summary(date)
-    self.where("datetoday =? ", date).order(:in)
   end
 
   def summary_of_current_month(month = Time.now.month)
