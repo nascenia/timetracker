fa418fae55b6389df6466bf7d9b7fec22c0906b1
Khalid <khalid@nascenia.com>
2014-11-19 18:17:09 +0600

    testing restrict user by ip

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  before_action :restrict_access
+
   def google_oauth2
     if request.env["omniauth.auth"][:info][:email].split('@').last == "nascenia.com" || request.env["omniauth.auth"][:info][:email].split('@').last == "bdipo.com"
       @user = User.find_for_google_oauth2(request.env["omniauth.auth"])
@@ -13,6 +15,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       flash[:notice] = "Email domain must be either 'nascenia.com' or 'bdipo.com'."
       redirect_to root_path
     end
+  end
 
+  def restrict_access
+    @user = User.find_for_google_oauth2(request.env["omniauth.auth"])
+    if @user.email == 'khalid@nascenia.com' && request.remote_ip != '192.168.1.10'
+      flash[:notice] = "Access denied!"
+      redirect_to root_path
+    end
   end
 end
