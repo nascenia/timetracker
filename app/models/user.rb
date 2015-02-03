5b56b55f3241198d66213e6825d50eb9cbbc8194
Khalid <khalid@nascenia.com>
2015-02-03 09:28:46 +0600

    Excluded sunday from calculating avg time & avg in time.

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,15 +40,16 @@ class User < ActiveRecord::Base
     self.attendances.where("MONTH(datetoday) =?", month).sum(:total_hours)
   end
 
-  def monthly_average_hour(month)
-    total_days_spent_in_office = self.attendances.where("MONTH(datetoday) =? AND attendances.total_hours IS NOT NULL", month).count("datetoday", :distinct => true)
+  def monthly_average_hour(month, sunday = 6)
+    total_days_spent_in_office = self.attendances.where("MONTH(datetoday) =? AND WEEKDAY(datetoday) !=? AND attendances.total_hours IS NOT NULL", month, sunday).count("datetoday", :distinct => true)
+    monthly_total_hour = self.attendances.where("MONTH(datetoday) =? AND WEEKDAY(datetoday) !=? ", month, sunday).sum(:total_hours)
     if total_days_spent_in_office > 0
-      monthly_total_hour(month) / total_days_spent_in_office
+      monthly_total_hour / total_days_spent_in_office
     end
   end
 
-  def monthly_average_in_time(month)
-    total = self.attendances.where("MONTH(datetoday) =? AND first_entry =? ", month, true).average("TIME_TO_SEC(attendances.in)")
+  def monthly_average_in_time(month, sunday = 6)
+    total = self.attendances.where("MONTH(datetoday) =? AND WEEKDAY(datetoday) !=? AND first_entry =? ", month, sunday, true).average("TIME_TO_SEC(attendances.in)")
     if total.present?
       Time.at(total).utc.strftime("%I:%M %p")
     end
