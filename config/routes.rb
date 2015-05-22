adfb0322c8bcd03d37eb7ca59399079f0c04f1a0
Khalid <khalid@nascenia.com>
2015-05-22 14:35:57 +0600

    Added Active admin panel.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Internal::Application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :attendances do
     get 'search_daily_attendance', :on => :collection
     get 'update_salaat_time', :on => :collection
