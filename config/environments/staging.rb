294fdd1a518e8154b907944309bd3374e5572523
Khalid <khalid@nascenia.com>
2015-12-23 15:47:20 +0600

    Added host name in staging.rb

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -64,6 +64,9 @@ Internal::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+
+  config.action_controller.default_url_options = { host: 'timetracker.nascenia.com' }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address  => "smtp.gmail.com",
