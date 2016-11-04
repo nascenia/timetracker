a7127f4b024d415d08adaaa6bc6d130a5c6f3030
Masud Rana <masud@nascenia.com>
2016-11-04 15:00:55 +0600

    - Added internal application services info to the yml configuration file - Added yml configuration to the view template

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,17 +1,5 @@
 class Service < ActiveRecord::Base
 
-  OTHERS = {
-      tp: {
-          name: 'Team planner',
-          url: 'http://test.teamplanner.nascenia.com/'
-      },
-      kb: {
-          name: 'Knowledge base',
-          url: 'http://kb.nascenia.com/'
-      },
-      fs: {
-          name: 'File server',
-          url: 'http://192.168.1.2/'
-      }
-  }
+  INTERNAL = CONFIG['services']
+
 end
