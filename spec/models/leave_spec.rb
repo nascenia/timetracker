f46e514f17ec4e5d9b15ebeace0339f7c150d96b
Masud Rana <masud@nascenia.com>
2017-01-18 17:22:21 +0600

    - Removed leave variable from leave spec

--- spec/models/leave_spec.rb
+++ spec/models/leave_spec.rb
@@ -11,98 +11,62 @@ RSpec.describe Leave, type: :model do
     end
   end
 
-  LEAVE = {
-    CASUAL: 1,
-    MEDICAL: 2,
-    UNANNOUNCED: 3,
-
-    LEAVE_TYPES: [
-      ['Casual Leave', 1],
-      ['Medical Leave', 2]
-    ],
-
-    ACCEPTED: 1,
-    REJECTED: 2,
-    PENDING: 3,
-
-    HOURS_FOR_ONE_DAY: 8,
-    HOURS_FOR_HALF_DAY: 4,
-
-    LEAVE_STATUSES: [
-      ['Approved', 1],
-      ['Rejected', 2],
-      ['Pending', 3]
-    ]
-  }
-
   describe 'validations' do
     it 'should represent casual type' do
-      expect(Leave::CASUAL).to eq(LEAVE[:CASUAL])
+      expect(Leave::CASUAL).to eq(1)
     end
 
     it 'should represent medical type' do
-      expect(Leave::MEDICAL).to eq(LEAVE[:MEDICAL])
+      expect(Leave::MEDICAL).to eq(2)
     end
 
     it 'should represent unannounced type' do
-      expect(Leave::UNANNOUNCED).to eq(LEAVE[:UNANNOUNCED])
+      expect(Leave::UNANNOUNCED).to eq(3)
     end
 
     it 'should define casual leave' do
-      expect(Leave::LEAVE_TYPES[0][0]).to eq(LEAVE[:LEAVE_TYPES][0][0])
-      expect(Leave::LEAVE_TYPES[0][1]).to eq(LEAVE[:LEAVE_TYPES][0][1])
+      expect(Leave::LEAVE_TYPES[0][0]).to eq('Casual Leave')
+      expect(Leave::LEAVE_TYPES[0][1]).to eq(1)
     end
 
     it 'should define medical leave' do
-      expect(Leave::LEAVE_TYPES[1][0]).to eq(LEAVE[:LEAVE_TYPES][1][0])
-      expect(Leave::LEAVE_TYPES[1][1]).to eq(LEAVE[:LEAVE_TYPES][1][1])
+      expect(Leave::LEAVE_TYPES[1][0]).to eq('Medical Leave')
+      expect(Leave::LEAVE_TYPES[1][1]).to eq(2)
     end
 
     it 'should represent accepted state' do
-      expect(Leave::ACCEPTED).to eq(LEAVE[:ACCEPTED])
+      expect(Leave::ACCEPTED).to eq(1)
     end
 
     it 'should represent rejected state' do
-      expect(Leave::REJECTED).to eq(LEAVE[:REJECTED])
+      expect(Leave::REJECTED).to eq(2)
     end
 
     it 'should represent pending state' do
-      expect(Leave::PENDING).to eq(LEAVE[:PENDING])
+      expect(Leave::PENDING).to eq(3)
     end
 
     it 'should represent one day office hours' do
-      expect(Leave::HOURS_FOR_ONE_DAY).to eq(LEAVE[:HOURS_FOR_ONE_DAY])
+      expect(Leave::HOURS_FOR_ONE_DAY).to eq(8)
     end
 
     it 'should represent half day office hours' do
-      expect(Leave::HOURS_FOR_HALF_DAY).to eq(LEAVE[:HOURS_FOR_HALF_DAY])
+      expect(Leave::HOURS_FOR_HALF_DAY).to eq(4)
     end
 
     it 'should represent leave approved status' do
-      expect(Leave::LEAVE_STATUSES[0][0]).to eq(LEAVE[:LEAVE_STATUSES][0][0])
-      expect(Leave::LEAVE_STATUSES[0][1]).to eq(LEAVE[:LEAVE_STATUSES][0][1])
+      expect(Leave::LEAVE_STATUSES[0][0]).to eq('Approved')
+      expect(Leave::LEAVE_STATUSES[0][1]).to eq(1)
     end
 
     it 'should represent leave rejected status' do
-      expect(Leave::LEAVE_STATUSES[1][0]).to eq(LEAVE[:LEAVE_STATUSES][1][0])
-      expect(Leave::LEAVE_STATUSES[1][1]).to eq(LEAVE[:LEAVE_STATUSES][1][1])
+      expect(Leave::LEAVE_STATUSES[1][0]).to eq('Rejected')
+      expect(Leave::LEAVE_STATUSES[1][1]).to eq(2)
     end
 
     it 'should represent leave pending status' do
-      expect(Leave::LEAVE_STATUSES[2][0]).to eq(LEAVE[:LEAVE_STATUSES][2][0])
-      expect(Leave::LEAVE_STATUSES[2][1]).to eq(LEAVE[:LEAVE_STATUSES][2][1])
-    end
-  end
-
-  # TODO: leave tracker related features are not yet full functional,
-  # TODO: first this have to be implemented then test code should be updated
-  describe 'update_leave_tracker' do
-    let(:user) { create :user, email: 'khalid@nascenia.com' }
-    let(:leave) { create :leave, user_id: user.id }
-
-    it 'should update leave tracker' do
-      # leave.update_leave_tracker
-      # expect('a').to eq('a')
+      expect(Leave::LEAVE_STATUSES[2][0]).to eq('Pending')
+      expect(Leave::LEAVE_STATUSES[2][1]).to eq(3)
     end
   end
 
