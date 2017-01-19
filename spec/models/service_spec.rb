cc477bd155e9ebb158e04212e44ea95eaf5fc8ba
Masud Rana <masud@nascenia.com>
2017-01-19 10:36:14 +0600

    - Fix service spec issues due to the internal services moved to the application.yml file

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,35 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Service, type: :model do
-  OTHERS = {
-      tp: {
-          name: 'Team planner',
-          url: 'http://test.teamplanner.nascenia.com/'
-      },
-      kb: {
-          name: 'Knowledge base',
-          url: 'http://kb.nascenia.com/'
-      },
-      fs: {
-          name: 'File server',
-          url: 'http://192.168.1.2/'
-      }
-  }
-
   describe 'validation' do
-    it 'should team planner' do
-      expect(Service::OTHERS[:tp][:name]).to eq(OTHERS[:tp][:name])
-      expect(Service::OTHERS[:tp][:url]).to eq(OTHERS[:tp][:url])
+    it 'should be team planner' do
+      expect(Service::INTERNAL['team_planner']['name']).to eq('Team planner')
+      expect(Service::INTERNAL['team_planner']['url']).to eq('http://test.teamplanner.nascenia.com/')
     end
 
-    it 'should knowledge base' do
-      expect(Service::OTHERS[:kb][:name]).to eq(OTHERS[:kb][:name])
-      expect(Service::OTHERS[:kb][:url]).to eq(OTHERS[:kb][:url])
+    it 'should be knowledge base' do
+      expect(Service::INTERNAL['knowledge_base']['name']).to eq('Knowledge base')
+      expect(Service::INTERNAL['knowledge_base']['url']).to eq('http://kb.nascenia.com/')
     end
 
-    it 'should File Server' do
-      expect(Service::OTHERS[:fs][:name]).to eq(OTHERS[:fs][:name])
-      expect(Service::OTHERS[:fs][:url]).to eq(OTHERS[:fs][:url])
+    it 'should be file Server' do
+      expect(Service::INTERNAL['file_server']['name']).to eq('File server')
+      expect(Service::INTERNAL['file_server']['url']).to eq('http://192.168.1.2/')
     end
   end
 end
