282ec0bc854e029bc29fe1444a4b8cc4afd28253
alvi <alvi.mahadi@bdipo.com>
2017-03-23 16:06:56 +0600

    Humanized total and average time

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -136,7 +136,7 @@ class Attendance < ActiveRecord::Base
       total_attendance = total_attendance - ( half_day_count * 0.5 )
     end
     average_hours = total_attendance > 0 ? total_hours / total_attendance : 0
-    average_hours.round(2)
+    (average_hours.to_i).to_s + ' Hours ' + (((average_hours % 1)* 60).round(0)).to_s + ' Minutes '
   end
 
   #
@@ -156,5 +156,10 @@ class Attendance < ActiveRecord::Base
 
     average_time = '%s:%s' % average_minutes.divmod(60).map(&:to_i)
     Time.parse(average_time).strftime('%I:%M %p')
+  end
+
+  def self.get_humanize_total_hour monthly_attendances
+    total_hours = self.monthly_total_hours(monthly_attendances)
+    (total_hours.to_i).to_s + ' Hours ' + (((total_hours % 1)* 60).round(0)).to_s + ' Minutes '
   end
 end
