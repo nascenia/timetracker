9eb2d057d293e6053272a00a932c3efb4031d948
Zahidul Haque <iisowrovii@gmail.com>
2017-06-20 17:56:06 +0600

    remove corn jobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,10 +2,10 @@ every :day, at: '6.00 pm' do
   runner 'User.create_unannounced_leave', output: { error: 'log/error.log', standard: 'log/cron.log' }
 end
 
-every 1.year, at: 'December 31st 11:58pm' do
-  runner 'LeaveTracker.update_leave_tracker_yearly'
-end
+# every 1.year, at: 'December 31st 11:58pm' do
+#   runner 'LeaveTracker.update_leave_tracker_yearly'
+# end
 
-every 1.year, at: 'January 1st 12:00am' do
-  runner 'LeaveTracker.initialize_every_leave_with_new_year'
-end
+# every 1.year, at: 'January 1st 12:00am' do
+#   runner 'LeaveTracker.initialize_every_leave_with_new_year'
+# end
