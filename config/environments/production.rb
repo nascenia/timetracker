753f9fede3b740df3564e8eea6b6c98c7d03020b
alvi2496 <alvi.utsab@gmail.com>
2017-09-20 12:41:23 +0600

    mailer setup in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,18 @@ Internal::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: 'gmail.com',
+      authentication: 'plain',
+      enable_starttls_auto: true,
+      user_name: CONFIG['mailer_email'],
+      password: CONFIG['mailer_password']
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
