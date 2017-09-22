6f5d252d5bfceeb670199ee5cc2d2ec8fc85a6aa
alvi2496 <alvi.utsab@gmail.com>
2017-09-22 16:09:19 +0600

    added host to default_url_option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Internal::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_option = { host: "timetracker.test.nascenia.com"}
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.delivery_method = :smtp
