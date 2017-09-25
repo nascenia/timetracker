8793be7159f533ef213ca7dd9f7d8d02c49adf56
alvi2496 <alvi.utsab@gmail.com>
2017-09-25 14:31:43 +0600

    set perform_deliveries to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,9 +65,10 @@ Internal::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: "timetracker.test.nascenia.com"}
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-
   config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.smtp_settings = {
       address: ENV["TT_MAILER_ADDRESS"],
       port: ENV["TT_MAILER_PORT"].to_i,
