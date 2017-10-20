84c60c06b6956390c154e267a15aef2b9b55ad97
alvi2496 <alvi.utsab@gmail.com>
2017-10-20 16:18:35 +0600

    changed the admin mail address

--- app/models/user.rb
+++ app/models/user.rb
@@ -181,7 +181,7 @@ class User < ActiveRecord::Base
         end
       end
       UserMailer.send_unannounced_leave_notification_to_user(leave).deliver
-      UserMailer.send_unannounced_leave_notification_to_admin(leave, 'alvi.mahadi@bdipo.com').deliver
+      UserMailer.send_unannounced_leave_notification_to_admin(leave, 'afroze@nascenia.com').deliver
     else
       Rails.logger.info "Unable to create unannounced leave for #{name}"
     end
