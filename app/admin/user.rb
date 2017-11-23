e40b3e8cb5bb23621ca45aaa84e0a4a3d348021b
alvi2496 <alvi.utsab@gmail.com>
2017-11-23 19:19:21 +0600

    remove password digest from active admin user show

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -32,6 +32,13 @@ ActiveAdmin.register User do
     actions
   end
 
+  show do
+    attributes_table :email, :name, :is_active, :role, :ttf_id, :sttf_id, :date_of_birth, :joining_date,
+                     :resignation_date, :personal_email, :present_address, :mobile_number, :alternate_contact,
+                     :permanent_address, :last_degree, :last_university, :passing_year, :emergency_contact_person_name,
+                     :emergency_contact_person_relation, :emergency_contact_person_relation,  :emergency_contact_person_number
+  end
+
   form do |f|
     f.inputs "User Details" do
       f.input :email
