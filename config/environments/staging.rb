f79e5d9a1829bad6eaaf4872634b3d80e97dcf27
Khalid <khalid@nascenia.com>
2017-12-27 14:51:25 +0600

    Fixing email sedning error

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -65,9 +65,11 @@ Internal::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host:  ENV['HOST'] }
-
+  config.action_mailer.default_url_options = { host: "timetracker.nascenia.com"}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: ENV["TT_MAILER_PORT"].to_i,
