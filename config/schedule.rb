e70dacc5d1fda1b03be86513fc0801abe54536fa
Khalid <khalid@nascenia.com>
2017-12-28 11:22:19 +0600

    updated scheduler syntax

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,10 +6,10 @@ every :day, :at => '4:00pm' do
   runner 'User.create_unannounced_leave', output: { error: 'log/error.log', standard: 'log/cron.log' }
 end
 
-every 1.year, :at => '12:01am' do
+every :year, :at => '12:01am' do
   runner 'LeaveTracker.update_leave_tracker_yearly'
 end
 
-every 1.year, :at => '12:10am' do
+every :year, :at => '12:10am' do
   runner 'LeaveTracker.initialize_every_leave_with_new_year'
 end
