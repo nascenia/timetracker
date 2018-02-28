6d74ec4ad179bd6f559163bc91b875aa41441014
saimun <imran.saimun@bdipo.com>
2018-02-28 11:26:38 +0600

    sort leaves by start date

--- app/controllers/leave_tracker_controller.rb
+++ app/controllers/leave_tracker_controller.rb
@@ -19,10 +19,10 @@ class LeaveTrackerController < ApplicationController
     @leave_tracker = @user.leave_tracker
     @leave_tracker.update_leave_tracker_daily
     if params[:month].present?
-      @leaves = @user.leaves.leaves_by_month(params[:month])
+      @leaves = @user.leaves.leaves_by_month(params[:month]).order('start_date DESC')
       @month = params[:month]
     else
-      @leaves = @user.leaves
+      @leaves = @user.leaves.order('start_date DESC')
       @month = nil
     end
   end
