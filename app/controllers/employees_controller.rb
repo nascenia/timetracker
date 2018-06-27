8f5c0d7104e893f54928ba5ca8eb46744835efd2
saimun <saimun2025@gmail.com>
2018-06-27 15:19:01 +0600

    filter added to employee list

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -3,21 +3,24 @@ class EmployeesController < ApplicationController
   layout 'time_tracker'
 
   def index
-    if current_user.is_admin?
-      @employees = User.where(:is_active => true).order(name: :asc)
-    else
-      @employees =  User.where(:is_active => true).order(name: :asc)
-    end
+    @employees =  User.all.order(name: :asc)
 
     # @employees.each do |employee|
     #   logger.info employee
     # end
     @employees = @employees.where('lower(name) LIKE ?', "%#{params[:name].strip.downcase}%") if params[:name].present?
     @employees = @employees.where('lower(email) LIKE ?', "%#{params[:email].strip.downcase}%") if params[:email].present?
+    logger.info "------------------------------------"
+    logger.info params
+    logger.info params[:employee_status]
+    logger.info params[:name]
+
     if params[:employee_status].present?
       @employees = @employees.published if params[:employee_status] == '0'
       @employees = @employees.active if params[:employee_status] == '1'
       @employees = @employees.inactive if params[:employee_status] == '2'
+      @employees = @employees.not_published if params[:employee_status] == '3'
+      @employees = @employees.not_register if params[:employee_status] == '4'
     end
   end
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -53,7 +53,9 @@ class User < ActiveRecord::Base
 
   scope :inactive, -> {where('is_active = ?', false)}
   scope :active, -> {where('is_active = ?', true)}
-  scope :published, -> { where(is_published: true) }
+  scope :published, -> { where(registration_status: 2) }
+  scope :not_published, -> { where(registration_status: 1) }
+  scope :not_register, -> { where(registration_status: 0) }
   scope :ttf, -> {where('role = ?', User::TTF)}
   scope :super_ttf, -> {where('role = ?', User::SUPER_TTF)}
   scope :employees, -> {where('role = ?', User::EMPLOYEE)}
