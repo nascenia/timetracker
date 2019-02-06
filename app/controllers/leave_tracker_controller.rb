e6f2430d74e259548b1036051587427972271264
gbriyad <gbriyad@gmail.com>
2019-02-06 12:39:52 +0600

    remove 'rollbacked leaves' from individual employee's leave

--- app/controllers/leave_tracker_controller.rb
+++ app/controllers/leave_tracker_controller.rb
@@ -18,10 +18,11 @@ class LeaveTrackerController < ApplicationController
     end
     @leave_tracker = @user.leave_tracker
     @leave_tracker.update_leave_tracker_daily
+
     if params[:year].present?
-      @leaves = @user.leaves.leaves_by_year(params[:year]).order('start_date DESC, created_at DESC')
+      @leaves = @user.leaves.not_rollbacked_leaves.leaves_by_year(params[:year]).order('start_date DESC, created_at DESC')
     else
-      @leaves = @user.leaves.order('start_date DESC, created_at DESC')
+      @leaves = @user.leaves.not_rollbacked_leaves.order('start_date DESC, created_at DESC')
     end
     @leave = Leave.new
     @resignation_error_msg = 'The employee has Casual/ Medical leaves after Last day of working!' if @leave_tracker.casual_or_medical_leave_present_after_resignation
