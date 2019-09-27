e002c2b4b753a14082ab19f3c491c22b9b0482f0
Sarwar <pritom_kushtia@yahoo.com>
2019-09-27 11:35:25 +0600

    ADDED ADMIN PREVILLAGE Added total calculation Only admin can edit/create project Signout autumatically if new timesheet comes from checkout

--- app/controllers/timesheets_controller.rb
+++ app/controllers/timesheets_controller.rb
@@ -79,9 +79,9 @@ class TimesheetsController < ApplicationController
                                 flash[:notice] = 'You did not log in today.'
                             end
                         end
-                        redirect_to root_path
+                        redirect_to timesheets_path(selected_index: 0,start_date: (Time.now-14.days).strftime("%Y/%m/%d") ,end_date: Time.now.strftime("%Y/%m/%d"))
                     else
-                        redirect_to root_path
+                        redirect_to timesheets_path(selected_index: 0,start_date: (Time.now-14.days).strftime("%Y/%m/%d") ,end_date: Time.now.strftime("%Y/%m/%d"))
                     end
                 else
                     flash[:alert] = 'failed'
@@ -101,34 +101,45 @@ class TimesheetsController < ApplicationController
     end
 
     def edit
-        @total_hours = 14
         @timesheets = Timesheet.find(params[:id])
-        @projects = Project.all
-        user_timesheet = current_user.timesheets.where(date: Timesheet.find(params[:id]).date)
-        total_hours_hash=user_timesheet.sum(:hours)
-        total_minute_hash=user_timesheet.sum(:minutes)
-        # @total_hours -=total_hours_hash + (total_minute_hash/60)
-        @total_mins = 3
-        @total_hours = (14 - (total_hours_hash+total_minute_hash/60))+Timesheet.find(params[:id]).hours
-        if(14 - (total_hours_hash+total_minute_hash/60) ==0)
-            total_min = @total_hours*60
-            @total_mins = (total_min%60)/15
+        if @timesheets.date.to_date>=Time.now.to_date-35
+             @total_hours = 14
+             projects = current_user.projects
+             p '#####CU project'
+             projects.each do |project|
+                 p project
+             end
+             p '#####CU project'
+             @projects = Project.where.not(id: current_user.projects)
+             user_timesheet = current_user.timesheets.where(date: Timesheet.find(params[:id]).date)
+             total_hours_hash=user_timesheet.sum(:hours)
+             total_minute_hash=user_timesheet.sum(:minutes)
+             # @total_hours -=total_hours_hash + (total_minute_hash/60)
+             @total_mins = 3
+             @total_hours = (14 - (total_hours_hash+total_minute_hash/60))+Timesheet.find(params[:id]).hours
+             if(14 - (total_hours_hash+total_minute_hash/60) ==0)
+                 total_min = @total_hours*60
+                 @total_mins = (total_min%60)/15
+             end
+        else
+            flash[:alert] = 'You cannot edit before 35 days'
+            redirect_to timesheets_path(selected_index: 0,start_date: (Time.now-14.days).strftime("%Y/%m/%d") ,end_date: Time.now.strftime("%Y/%m/%d"))
         end
     end
 
     def update
         @timesheets = Timesheet.find(params[:id])
-        if @timesheets.date.to_date>=Time.now.to_date-35
+        if timesheet_params[:date].to_date>=Time.now.to_date-35
             if @timesheets.save && @timesheets.update(timesheet_params)
                 redirect_to timesheets_path(selected_index: 0,start_date: (Time.now-14.days).strftime("%Y/%m/%d") ,end_date: Time.now.strftime("%Y/%m/%d"))
             else
                 flash[:alert] = 'failed'
-                redirect_to root_path
+                redirect_to timesheets_path(selected_index: 0,start_date: (Time.now-14.days).strftime("%Y/%m/%d") ,end_date: Time.now.strftime("%Y/%m/%d"))
             end
         else
             session[:is_from_checkout] = 0
             flash[:alert] = 'You cannot update before 35 days'
-            redirect_to new_timesheet_path
+            redirect_to timesheets_path(selected_index: 0,start_date: (Time.now-14.days).strftime("%Y/%m/%d") ,end_date: Time.now.strftime("%Y/%m/%d"))
         end
 
     end
