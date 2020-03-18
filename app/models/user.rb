b67f0228f7b976757494cd6ed635c007fbd9528e
Sarwar <pritom_kushtia@yahoo.com>
2020-03-18 17:47:22 +0600

    Solve the timesheet calculation problem of multiple day leave

--- app/models/user.rb
+++ app/models/user.rb
@@ -252,7 +252,7 @@ class User < ActiveRecord::Base
                 expected_productive_time_to_in_office  = (date_difference*8)-4
               end
           else
-            date_diff_db = (leave_count_by_date_individual.end_date.to_date - leave_count_by_date_individual.start_date.to_date).to_i
+            date_diff_db = (leave_count_by_date_individual.end_date - leave_count_by_date_individual.start_date).to_i
             expected_time_to_spend_in_office  = (date_difference-date_diff_db)*9
             expected_productive_time_to_in_office  = (date_difference-date_diff_db)*8
 
