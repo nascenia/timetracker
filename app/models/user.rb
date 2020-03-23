68dcd6526d66bb54fc21b0bd9b6fcf3b330d2bb4
Sarwar <pritom_kushtia@yahoo.com>
2020-03-23 13:25:25 +0600

    Solve the timesheet calculation problem of multiple day leave print log

--- app/models/user.rb
+++ app/models/user.rb
@@ -242,7 +242,7 @@ class User < ActiveRecord::Base
               if leave_count_by_date_individual.half_day == Leave::FULL_DAY
                 if leave_count_by_date_individual.end_date >= options2[:end_date].to_date
                   local_date_diff = local_date_diff+((options2[:end_date].to_date - leave_count_by_date_individual.start_date)+1).to_i
-                  logger.info user[:name]+"  END DATE : "+ leave_count_by_date_individual.end_date+" *******************************   245  "+"  START DATE : "+ leave_count_by_date_individual.start_date+" ****"+  local_date_diff.to_s
+                  logger.info user[:name]+"  END DATE : "+ leave_count_by_date_individual.end_date.to_s+" *******************************   245  "+"  START DATE : "+ leave_count_by_date_individual.start_date.to_s+" ****"+  local_date_diff.to_s
                   expected_time_to_spend_in_office  = (date_difference-local_date_diff)*9
                   expected_productive_time_to_in_office  = (date_difference-local_date_diff)*8
                 else
