970d469487efca8d8f85c2608bfa5f22704f62c2
Sarwar <pritom_kushtia@yahoo.com>
2020-03-24 13:14:19 +0600

    Add Mr. Shamim satamim79@gmail.com email as while listed

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def google_oauth2
     domain = request.env["omniauth.auth"][:extra][:id_info][:email].split('@').last
-    if  CONFIG['domain_whitelist'].include? domain
+    if CONFIG['domain_whitelist'].include? domain
       @user = User.find_for_google_oauth2(request.env["omniauth.auth"])
       if @user.persisted?
         flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
@@ -12,8 +12,19 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         redirect_to new_user_registration_url and return
       end
     else
-      flash[:notice] = "Email domain must be either 'nascenia.com' or 'bdipo.com'."
-      redirect_to root_path
+      if request.env["omniauth.auth"][:extra][:id_info][:email].to_s == 'satamim79@gmail.com'
+        @user = User.find_for_google_oauth2(request.env["omniauth.auth"])
+        if @user.persisted?
+          flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+          sign_in_and_redirect @user, :event => :authentication
+        else
+          session["devise.google_data"] = request.env["omniauth.auth"]
+          redirect_to new_user_registration_url and return
+        end
+        else
+          flash[:notice] = "Email domain must be either 'nascenia.com' or 'bdipo.com'."
+          redirect_to root_path
+      end
     end
   end
 end
