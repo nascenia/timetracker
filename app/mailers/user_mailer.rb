35f13760a6ee90cd6eca0b0d054c6f48309aeb55
notorious94 <akib94dhk@gmail.com>
2020-07-28 16:14:50 +0600

    edit info mail functionality enabled

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -203,8 +203,9 @@ class UserMailer < ActionMailer::Base
   def send_approval_or_rejection_notification_of_employee_registration_to_hr(user)
     @user = user
     subject = 'Action required: Change(s) detected in employee information and approval needed'
-    #mail to: 'hr@nascenia.com', subject: subject
-    mail to: 'akib94dhk@gmail.com', subject: subject
+
+    mail to: 'hr@nascenia.com', subject: subject
+    mail to: ENV['TT_CEO_EMAIL'], subject: subject
     true
   rescue => e
     logger.error e.message
