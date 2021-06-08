fc9acdc96fca0c264f3ca9abf06f24a8d4140035
Md. Masud Rana <masudcsesust04@gmail.com>
2021-06-08 18:14:29 +0600

    - Modified seed data and added designation seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Salaat.create(waqt: 'Zohr', time: '1:30')
-Salaat.create(waqt: 'Asor', time: '4:15')
-Salaat.create(waqt: 'Magrib', time: '5:30')
+
+#Salaat.create(waqt: 'Zohr', time: '1:30')
+#Salaat.create(waqt: 'Asor', time: '4:15')
+#Salaat.create(waqt: 'Magrib', time: '5:30')
 
 #Create present year and mark it as present
-LeaveYear.find_or_create_by(year: Date.today.year.to_s, present: true)
+if LeaveYear.count.zero?
+  p 'Creating LeaveYear...'
+  LeaveYear.find_or_create_by(year: Date.today.year.to_s, present: true)
+end
+
+if Designation.count.zero?
+  p 'Creating employee designations...'
+  Designation.create([
+    { title: 'Intern', description: 'Internship', published: true },
+    { title: 'Junior Software Engineer', description: 'Junior software engineer', published: true },
+    { title: 'Software Engineer', description: 'Software engineer', published: true },
+    { title: 'Senior Software Engineer', description: 'Senior software engineer', published: true }
+  ])
+end
