5abda1208565b4e9e8167d5a0f0f8dac73bd3110
Md. Masud Rana <masudcsesust04@gmail.com>
2021-06-08 18:13:52 +0600

    - Added model and migration to manage employees designation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210531062710) do
+ActiveRecord::Schema.define(version: 20210608114042) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -75,6 +75,14 @@ ActiveRecord::Schema.define(version: 20210531062710) do
 
   add_index "comments", ["leave_id"], name: "index_comments_on_leave_id", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
+
+  create_table "designations", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.boolean  "published"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "exclusion_dates", force: true do |t|
     t.date     "date",          null: false
