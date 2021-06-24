fd1f1ba30323561889bc645d09f99b7d4089534f
Md. Masud Rana <masudcsesust04@gmail.com>
2021-06-24 19:29:14 +0600

    - Removed server site employee id format validation

--- app/models/pre_registration.rb
+++ app/models/pre_registration.rb
@@ -4,7 +4,7 @@ class PreRegistration < ActiveRecord::Base
   validates_uniqueness_of :companyEmail, allow_blank: true, allow_nil: true
   
   validates :employee_id, presence: true, uniqueness: true, length: { is: 5 }
-  validates :employee_id, format: { with: /^A[A-Za-z0-9]+\z/, message: 'Only letter and number are allowed', multiline: true }
+  #validates :employee_id, format: { with: /[^a-zA-Z0-9]/, message: 'Only letter and number are allowed', multiline: true }
 
   def update_from_user
     step_no = 3
