1ace0bba24d4991f7246c78cc90339c2caa3d566
Md. Masud Rana <masudcsesust04@gmail.com>
2021-10-18 17:20:15 +0600

    - Moved google oauth2 configuration to application yaml file

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,20 +5,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if CONFIG['domain_whitelist'].include? domain
       @user = User.find_for_google_oauth2(request.env["omniauth.auth"])
       if !@user.nil?
-      if @user.persisted?
-        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-        sign_in_and_redirect @user, :event => :authentication
-      else
-        session["devise.google_data"] = request.env["omniauth.auth"]
-        redirect_to new_user_registration_url and return
-      end
-      else
-        flash[:notice] = "Contact with admin please "
-        redirect_to root_path and return
-      end
-    else
-      if request.env["omniauth.auth"][:extra][:id_info][:email].to_s == 'satamim79@gmail.com'
-        @user = User.find_for_google_oauth2(request.env["omniauth.auth"])
         if @user.persisted?
           flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
           sign_in_and_redirect @user, :event => :authentication
@@ -26,9 +12,25 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
           session["devise.google_data"] = request.env["omniauth.auth"]
           redirect_to new_user_registration_url and return
         end
+      else
+        flash[:notice] = "Contact with admin please "
+        redirect_to root_path and return
+      end
+    else
+      if request.env["omniauth.auth"][:extra][:id_info][:email].to_s == 'satamim79@gmail.com'
+        @user = User.find_for_google_oauth2(request.env["omniauth.auth"])
+        if !@user.nil?
+          if @user.persisted?
+            flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+            sign_in_and_redirect @user, :event => :authentication
+          else
+            session["devise.google_data"] = request.env["omniauth.auth"]
+            redirect_to new_user_registration_url and return
+          end
         else
           flash[:notice] = "Email domain must be either 'nascenia.com' or 'bdipo.com'."
           redirect_to root_path
+        end
       end
     end
   end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env == "development"
-    provider :google_oauth2, "261859180890-omko6rmcr9equhamir68n3a7c0dn9176.apps.googleusercontent.com", "i52c0WOc1bmuj59C7y0KA3NQ"
-  elsif Rails.env == "production"
-    provider :google_oauth2, "581211922187-na063css1t5ca6a3mutpd1vua9elp52f.apps.googleusercontent.com", "QfYmBr8qQwQQ0M3ZjHmBx2LW"
-  elsif Rails.env == "staging"
-    provider :google_oauth2, "581211922187-na063css1t5ca6a3mutpd1vua9elp52f.apps.googleusercontent.com", "QfYmBr8qQwQQ0M3ZjHmBx2LW"
-  end
+  provider :google_oauth2, CONFIG['google_oauth2']['client_id'], CONFIG['google_oauth2']['client_secret']
 end
