c73d7d770caf4c729172a334c0533f551fe2a43a
Md. Masud Rana <masudcsesust04@gmail.com>
2021-11-25 11:57:00 +0600

    - Updated view template and javascript navigations for input fields

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -3,7 +3,7 @@ class Goal < ActiveRecord::Base
     belongs_to  :goal_category
     belongs_to  :user
 
-    attr_accessor :personal_or_team
+    attr_accessor :personal_or_team, :time_period
     
     validates   :title, presence: true
     validates   :point, presence: true, numericality: true
