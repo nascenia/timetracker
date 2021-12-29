84ae224653bfcff81890004e467d6f5d1041e3b8
Md. Masud Rana <masudcsesust04@gmail.com>
2021-12-29 16:46:34 +0600

    - KPI template to kpi item mappingissue fix. Kpi assignment view pages update

--- app/admin/kpi_item.rb
+++ app/admin/kpi_item.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register KpiItem do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params [:title, :description], :of, :attributes, :on, :model
+permit_params [:kpi_template_id, :title, :description], :of, :attributes, :on, :model
 #
 # or
 #
