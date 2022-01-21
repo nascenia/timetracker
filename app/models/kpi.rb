28034c1d5f71ee3f068ac5c050c3d14657764ce9
Md. Masud Rana <masudcsesust04@gmail.com>
2022-01-21 17:44:18 +0600

    - Updated score calculation as instance method

--- app/models/kpi.rb
+++ app/models/kpi.rb
@@ -1,6 +1,12 @@
 class Kpi < ActiveRecord::Base
 
     STATUSES = { inreview: 0, approved: 1 }
+    QUATERS   = [
+      { start_month: 1, end_month: 3 }, 
+      { start_month: 4, end_month: 6 }, 
+      { start_month: 7, end_month: 9 },
+      { start_month: 10, end_month: 12 }
+    ]
 
     belongs_to  :user
 
@@ -9,6 +15,14 @@ class Kpi < ActiveRecord::Base
     scope :by_date_range, -> (start_date, end_date) { 
       where("start_date >= ? AND end_date <= ?", Date.parse(start_date), Date.parse(end_date)) 
     }
+
+    def score
+      kpi = self
+      score = 0
+      data  = JSON.parse(kpi.data)
+      score = (data.sum { |kpi| kpi["score"] } / data.size).round(2)
+      score
+    end
 
     def self.search(params = {}, user_id)
       return [] if user_id.blank? && params.blank?
