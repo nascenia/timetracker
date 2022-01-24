14f1bc2cd9bba2e3d7bef06221b856e56b605254
Md. Masud Rana <masudcsesust04@gmail.com>
2022-01-24 14:22:28 +0600

    - Updated admin kpi view to show kpi items

--- app/admin/kpi.rb
+++ app/admin/kpi.rb
@@ -13,5 +13,43 @@ ActiveAdmin.register Kpi do
 #   permitted
 # end
 
+  index do
+    selectable_column
+    id_column
+    column :start_date
+    column :end_date
+    column :ttf_comment
+    column :team_member_comment
+    column :score
+    column :status do |kpi|
+      kpi.status == 0 ? 'In-Review' : 'Approved'
+    end
+    actions
+  end
 
+  show do
+    attributes_table do
+      row   :id
+      row   :user
+      row   :start_date
+      row   :end_date
+      row   :ttf_comment
+      row   :team_member_comment
+      row   :status do |kpi|
+        kpi.status == 0 ? 'In-Review' : 'Approved'
+      end
+    end
+
+    kpi_items = kpi.user.kpi_template.kpi_items
+
+    panel "KPI Items" do
+      table_for kpi_items do
+        column :title
+        column  :score do
+          '0.0'
+        end
+      end
+    end
+    
+  end
 end
