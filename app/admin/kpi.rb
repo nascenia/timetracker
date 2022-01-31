df83aee5d226e7261e420136adde9187e032bab6
Md. Masud Rana <masudcsesust04@gmail.com>
2022-01-31 17:18:27 +0600

    - Admin KPI edit feature implementation

--- app/admin/kpi.rb
+++ app/admin/kpi.rb
@@ -14,7 +14,7 @@ ActiveAdmin.register Kpi do
 # end
 
   actions :all, except: [:new]
-  
+
   filter  :user, as: :select, collection: proc { User.active }
   filter  :start_date
   filter  :end_date
@@ -68,6 +68,44 @@ ActiveAdmin.register Kpi do
           score = item.blank? ? '' : item['score']
           score
         end
+      end
+    end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :user, as: :select, collection: User.active
+      f.input :status,  as: :select, collection: Kpi::STATUSES
+      f.input :ttf_comment
+      f.input :team_member_comment
+      f.input :start_date
+      f.input :end_date
+    end
+    render partial: 'kpi_items', locals: { kpi: kpi, kpi_items: KpiItem.where(id: JSON.parse(kpi.data).map{|d| d['item_id']}) }
+    f.actions
+  end
+
+  controller do
+    def update
+      data      = []
+      item_ids  = params[:kpi_item_ids].map{|id| id.to_i}
+      item_ids.each_with_index do |item_id, i|
+        data << { item_id: item_id, score: params[:kpi_item_score][i].to_f }
+      end
+
+      kpi                     = Kpi.find(params[:id])
+      kpi.status              = params[:kpi][:status]
+      kpi.user_id             = params[:kpi][:user_id]
+      kpi.ttf_comment         = params[:kpi][:ttf_comment]
+      kpi.team_member_comment = params[:kpi][:team_member_comment]
+      kpi.start_date          = Date.strptime("#{params[:kpi]['start_date(1i)']}-#{params[:kpi]['start_date(2i)']}-#{params[:kpi]['start_date(3i)']}")
+      kpi.end_date            = Date.strptime("#{params[:kpi]['end_date(1i)']}-#{params[:kpi]['end_date(2i)']}-#{params[:kpi]['end_date(3i)']}")
+      kpi.data                = data.to_json
+
+      if kpi.save
+        redirect_to :admin_kpis, notice: 'KPI updated successfully.'
+      else
+        redirect_to :admin_kpis, notice: 'Sorry, Unable to updatee KPI'
       end
     end
   end
