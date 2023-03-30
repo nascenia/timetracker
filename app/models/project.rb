328564408f42b99cba95ec63cb85a6ae1db4ea6e
Md Masud Rana <masud@nascenia.com>
2023-03-30 16:57:28 +0600

    feat: project wise report

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
 class Project < ActiveRecord::Base
     has_many :timesheets
     has_and_belongs_to_many :users
+
+    scope :active, -> { where(is_active: true) }
+    scope :inactive, -> { where(is_active: false) }
 end
