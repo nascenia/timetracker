0d436a032ea49fa704053ed904b72ebcf73a39e5
Md Masud Rana <masud@nascenia.com>
2023-04-18 10:45:28 +0600

    refactor: kpi items cleanup

--- app/controllers/kpi_items_controller.rb
+++
@@ -1,9 +0,0 @@
-class KpiItemsController < InheritedResources::Base
-
-  private
-
-    def kpi_item_params
-      params.require(:kpi_item).permit(:title, :description, :score, :start_date, :end_date, :status)
-    end
-end
-
