14c906ba4539d69241b9232ad0af4c106c37edf3
Md Masud Rana <masud@nascenia.com>
2023-05-01 15:38:07 +0600

    fix: added user authentication callback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   # before_action :assign_salat_time
+
+  before_action :authenticate_user!
+  
   #
   # def assign_salat_time
   #   @salaat = Salaat.all
