0a95074121777af327420bc0d20d48c723409ab9
hmursaleen <habibulmursaleen@gmail.com>
2025-11-06 16:49:52 +0600

    Fixed: Project ID is shown instead of Project name in Timesheet page of admin panel

--- app/admin/timesheet.rb
+++ app/admin/timesheet.rb
@@ -30,4 +30,41 @@ ActiveAdmin.register Timesheet do
     column :updated_at
     actions
   end
+
+#
+#   ActiveAdmin’s filter DSL builds form fields in the sidebar and then hands the submitted values to Ransack, 
+#   which translates them into ActiveRecord queries. The as: and the particular filter name control:
+#   what input widget you see (select / text / date range / numeric),
+#   which Ransack predicate is used (e.g. _cont, _eq, _gteq),
+#   what gets sent in the request parameters (under q[...]),
+#   and therefore what SQL is generated.
+#
+
+  filter :user, as: :select, collection: -> { User.all.pluck(:name, :id) }
+  filter :project, as: :select, collection: -> { Project.all.pluck(:project_name, :id) }
+  filter :date
+  filter :description, as: :string
+  filter :created_at
+  filter :updated_at
+  filter :task,        as: :string
+  filter :ticket_number, as: :string
+  filter :ticket_link,   as: :string
+  filter :hours, as: :numeric, label: 'Hours'
+  filter :minutes, as: :numeric, label: 'Minutes'
+
+  form do |f|
+    f.inputs 'Timesheet Details' do
+      f.input :user, as: :select, collection: User.all.pluck(:name, :id)
+      f.input :project, as: :select, collection: Project.all.pluck(:project_name, :id)
+      f.input :date, as: :date_select, start_year: 2020, end_year: 2030
+      f.input :description
+      f.input :task
+      f.input :ticket_number
+      f.input :ticket_link
+      f.input :hours
+      f.input :minutes
+    end
+    f.actions
+  end
+
 end
