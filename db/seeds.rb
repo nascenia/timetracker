152509ce8d88ddc82200e3fb5ca4e9356cedf92b
Md. Reyanus Salehin <reyanus@nascenia.com>
2025-12-24 10:19:36 +0600

    commented out the seed file to keep the production environment safe from unexpected things to happen

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,45 +10,45 @@
 #Salaat.create(waqt: 'Asor', time: '4:15')
 #Salaat.create(waqt: 'Magrib', time: '5:30')
 
-if HolidayScheme.count.zero?
-  HolidayScheme.create([
-    { name: 'Nascenia General', active: true, leave_year_id: 1 },
-    { name: 'GJ and Biyeta', active: true, leave_year_id: 1 },
-  ])
-end
+# if HolidayScheme.count.zero?
+#   HolidayScheme.create([
+#     { name: 'Nascenia General', active: true, leave_year_id: 1 },
+#     { name: 'GJ and Biyeta', active: true, leave_year_id: 1 },
+#   ])
+# end
 
-if Weekend.count.zero?
-  Weekend.create([
-    { name: 'Nascenia Core: Sat & Sun', off_days: [:saturday, :sunday] },
-    { name: 'Admin: Sat', off_days: [:saturday] },
-    { name: 'Biyeta: Thu & Fri', off_days: [:thursday, :friday] }
-  ])
-end
+# if Weekend.count.zero?
+#   Weekend.create([
+#     { name: 'Nascenia Core: Sat & Sun', off_days: [:saturday, :sunday] },
+#     { name: 'Admin: Sat', off_days: [:saturday] },
+#     { name: 'Biyeta: Thu & Fri', off_days: [:thursday, :friday] }
+#   ])
+# end
 
-if LeaveYear.count.zero?
-  p 'Creating LeaveYear...'
-  LeaveYear.find_or_create_by(year: Date.today.year.to_s, present: true)
-end
+# if LeaveYear.count.zero?
+#   p 'Creating LeaveYear...'
+#   LeaveYear.find_or_create_by(year: Date.today.year.to_s, present: true)
+# end
 
-if Designation.count.zero?
-  p 'Creating employee designations...'
-  Designation.create([
-    { team: 'Developer', title: 'Intern', description: 'Internship', published: true },
-    { team: 'Developer', title: 'Junior Software Engineer', description: 'Junior software engineer', published: true },
-    { team: 'Developer', title: 'Software Engineer', description: 'Software engineer', published: true },
-    { team: 'Developer', title: 'Senior Software Engineer', description: 'Senior software engineer', published: true }
-  ])
-end
+# if Designation.count.zero?
+#   p 'Creating employee designations...'
+#   Designation.create([
+#     { team: 'Developer', title: 'Intern', description: 'Internship', published: true },
+#     { team: 'Developer', title: 'Junior Software Engineer', description: 'Junior software engineer', published: true },
+#     { team: 'Developer', title: 'Software Engineer', description: 'Software engineer', published: true },
+#     { team: 'Developer', title: 'Senior Software Engineer', description: 'Senior software engineer', published: true }
+#   ])
+# end
 
-if GoalCategory.count.zero?
-  p 'Creating goal categories...'
-  GoalCategory.create([
-    { title: 'Technical asset development', description: '', published: true },
-    { title: 'Develop skills',              description: '', published: true },
-    { title: 'Technical session',           description: '', published: true }
-  ])
-  p 'Done'
-end
+# if GoalCategory.count.zero?
+#   p 'Creating goal categories...'
+#   GoalCategory.create([
+#     { title: 'Technical asset development', description: '', published: true },
+#     { title: 'Develop skills',              description: '', published: true },
+#     { title: 'Technical session',           description: '', published: true }
+#   ])
+#   p 'Done'
+# end
 
 # -------- create Admin --------
 # AdminUser.find_or_create_by!(email: "reyanus_5@nascenia.com") do |admin|
