1b2c4d3fa37f593df92c6cbac55912a92dbcbbd7
Md. Reyanus Salehin <reyanus@nascenia.com>
2026-06-21 17:42:02 +0600

    increased request expiration time to 7 mins. one request invalids previous ones. disbling of cancel and close in modal button while requesting for reset password

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -13,4 +13,19 @@ class Users::PasswordsController < Devise::PasswordsController
       format.html { redirect_to new_user_session_path }
     end
   end
+
+  def edit
+    self.resource = resource_class.new
+    set_minimum_password_length
+    resource.reset_password_token = params[:reset_password_token]
+
+    if params[:reset_password_token].present?
+      user = resource_class.with_reset_password_token(params[:reset_password_token])
+      if user.nil?
+        @reset_token_error = 'Reset password token is invalid.'
+      elsif !user.reset_password_period_valid?
+        @reset_token_error = 'Reset password token has expired.'
+      end
+    end
+  end
 end
