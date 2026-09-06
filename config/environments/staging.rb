ed6ecfacc90c57cbcd12c301982a33eeba47d846
Md. Reyanus Salehin <reyanus@nascenia.com>
2026-09-06 14:10:18 +0600

    issue fix

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -65,7 +65,7 @@ Internal::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: "timetracker.nascenia.com"}
+  config.action_mailer.default_url_options = { host: "timetracker.nascenia.com", protocol: 'https' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
